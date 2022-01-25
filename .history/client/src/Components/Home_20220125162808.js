fa24c44ea7d39476ef3193b08a3dd451a86067a1
Guarav Wakchaure <gauravwakchaure1998@gmail.com>
2022-01-25 16:42:02 +0530

    carousel images changed
    done

--- .history/client/src/Components/Home_20220125162808.js
+++ .history/client/src/Components/Home_20220125162808.js
@@ -9,15 +9,15 @@ export default function Home() {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://wallpaperaccess.com/full/151827.jpg"
+                        src="https://images.jdmagicbox.com/quickquotes/images_main/duragres_glazed_vitrified_(grande_cementor_grey_600_x_1200_mm)_139481469379743800.jpg"
                         alt="First slide"
                         width="1200"
                         height="400"
                     />
-                    {/* <Carousel.Caption>
+                    <Carousel.Caption>
                         <h3 class="Flable">JAI JINENDRA</h3>
                         <p class="fldetail">Faith and Prayer both are invisible, but they make impossible things possible..</p>
-                    </Carousel.Caption> */}
+                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -28,38 +28,24 @@ export default function Home() {
                         height="400"
                     />
 
-                    {/* <Carousel.Caption>
+                    <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption> */}
+                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://kcd.org.uk/wordpress/wp-content/uploads/2020/04/WEBSITE-Risen-Ascended-Glorified-1200x400.jpg"
+                        src="https://images.jdmagicbox.com/quickquotes/images_main/duragres_glazed_vitrified_(grande_exp_claudia_white_600_x_1200_mm)_139481302012095500.jpg"
                         alt="Third slide"
                         width="1200"
                         height="400"
                     />
 
-                    {/* <Carousel.Caption>
+                    <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption> */}
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://wallpaperaccess.com/full/1119361.jpg"
-                        alt="Third slide"
-                        width="1200"
-                        height="400"
-                    />
-
-                    {/* <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption> */}
+                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
             </div>
--- .history/client/src/Components/Home_20220125163432.js
+++ .history/client/src/Components/Home_20220125163432.js
@@ -36,20 +36,6 @@ export default function Home() {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://kcd.org.uk/wordpress/wp-content/uploads/2020/04/WEBSITE-Risen-Ascended-Glorified-1200x400.jpg"
-                        alt="Third slide"
-                        width="1200"
-                        height="400"
-                    />
-
-                    {/* <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption> */}
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
                         src="https://wallpaperaccess.com/full/1119361.jpg"
                         alt="Third slide"
                         width="1200"
