19ff942ee40676fa5ef39c20bed6be27e6cf0023
Ratnesh kumar Pandey <ratneshkumarpandey76@gmail.com>
2022-01-26 13:09:34 +0530

    added to server file
    refrsh problem solved

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ app.use('/', route)
 
     if(process.env.NODE_ENV =="production"){
         app.use(express.static("client/build"));
+        app.get("/*", function(req, res) {
+            res.sendFile(path.join(__dirname, "./client/build/index.html"));
+          });
     }
 
     app.listen(PORT, function () {
