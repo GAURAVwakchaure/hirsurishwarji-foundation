682c46caabf888abd63e05750e2c09467a9bb371
Guarav Wakchaure <gauravwakchaure1998@gmail.com>
2022-01-27 14:41:41 +0530

    bootsrap alert for Eightstep done
    done

--- client/src/Components/EighthStep.js
+++ client/src/Components/EighthStep.js
@@ -33,65 +33,96 @@ export default function EighthStep() {
         var mgmedicalamount = document.getElementById('mgmedicalamount').value
         var mgmedinsurance = document.getElementById('mgmedinsurance').value
         console.log(mgeducation, mgexperience, mgloanamount, mghospitalname, mghospitaladdress, mgdiseasedetails, mgaprxexpenses, mgamountneeded, mgamtcontri, mgexistingmedaid, mgmedicalamount, mgmedinsurance)
-        // if (mgeducation == null || mgeducation === '' || mgeducation.length < 2  || mgeducation.length > 50) {
-        //     alert("Please Enter the MG Education !  Length of atleast of 2 characters and less than 50 characters")
-        //     setStep(8)
-        // } else if (mgexperience == null || mgexperience === '' || mgexperience.length < 2  || mgexperience.length > 50) {
-        //     alert("Enter MG Experience !  Length of atleast of 2 characters and less than 50 characters")
-        //     setStep(8)
 
-        // } 
         // else if (mgloanamount == null || mgloanamount === '' || mgloanamount.length < 1  || mgloanamount.length > 8) {
         //     alert("Enter MG Loan Amount !  Length of atleast of 2 digit and less than 8 digit")
         //     setStep(8)
 
         // } 
-        if (mghospitalname == null || mghospitalname === '' || mghospitalname.length < 2  || mghospitalname.length > 50) {
-            alert("Enter Hopital Name !  Length of atleast of 2 characters and less than 50 characters")
+        if (mghospitalname == null || mghospitalname === '') {
+            document.getElementById('emghospitalname').innerHTML="Please Enter Hopital Name !"
+            setStep(8)
+        }
+        else if(!isNaN(mghospitalname)){
+            document.getElementById('emghospitalname').innerHTML="Numbers are not allowed !"
+            setStep(8)
+        }
+        else if(mghospitalname.length < 2  || mghospitalname.length > 50){
+            document.getElementById('emghospitalname').innerHTML="Minimum 2 and Maximum 50 characters are allowed !"
+            setStep(8)
+        } 
+        else{
+            document.getElementById('emghospitalname').innerHTML=" "
+            setStep(8)
+        }
+        if (mghospitaladdress == null || mghospitaladdress === '') {
+            document.getElementById('emghospitaladdress').innerHTML="Please Enter Hopital Address !"
             setStep(8)
 
         } 
-        else if (mghospitaladdress == null || mghospitaladdress === '' || mghospitaladdress.length < 2  || mghospitaladdress.length > 50) {
-            alert("Enter Hospitaal Address !  Length of atleast of 2 characters and less than 50 characters")
+        else if(mghospitaladdress.length < 2  || mghospitaladdress.length > 50){
+            document.getElementById('emghospitaladdress').innerHTML="Minimum 2 and Maximum 50 characters are allowed !"
+            setStep(8)
+        }
+        else{
+            document.getElementById('emghospitaladdress').innerHTML=" "
+            setStep(8)
+        }
+        if (mgdiseasedetails == null || mgdiseasedetails === '') {
+            document.getElementById('emgdiseasedetails').innerHTML="Please Enter Disease Details !"
+            setStep(8)
+        } 
+        else if(mgdiseasedetails.length < 2  || mgdiseasedetails.length > 50){
+            document.getElementById('emgdiseasedetails').innerHTML="Minimum 2 and Maximum 50 characters are allowed !"
+            setStep(8)
+        }
+        else{
+            document.getElementById('emgdiseasedetails').innerHTML=" "
+            setStep(8)
+        }
+        if(mgaprxexpenses == null || mgaprxexpenses === '') {
+            document.getElementById('emgaprxexpenses').innerHTML="Please Enter Approx Expenses !"
+            setStep(8)
+        }
+        else if(mgaprxexpenses.length < 1  || mgaprxexpenses.length > 8){
+            document.getElementById('emgaprxexpenses').innerHTML="Minimum 1 and Maximum 8 digits are allowed !"
+            setStep(8)
+        } 
+        else{
+            document.getElementById('emgaprxexpenses').innerHTML=" "
+            setStep(8)
+        }
+        if (mgamountneeded == null || mgamountneeded === '') {
+            document.getElementById('emgamountneeded').innerHTML="Please Enter Amount Needed !"
             setStep(8)
 
         } 
-        else if (mgdiseasedetails == null || mgdiseasedetails === '' || mgdiseasedetails.length < 2  || mgdiseasedetails.length > 50) {
-            alert("Enter Disease Details !  Length of atleast of 2 characters and less than 50 characters")
+        else if(mgamountneeded.length < 1  || mgamountneeded.length > 8){
+            document.getElementById('emgamountneeded').innerHTML="Minimum 1 and Maximum 8 digits are allowed !"
             setStep(8)
-
-        } 
-        else if (mgaprxexpenses == null || mgaprxexpenses === '' || mgaprxexpenses.length < 1  || mgaprxexpenses.length > 8) {
-            alert("Enter Approx Expenses !  Length of atleast of 1 digit and less than 8 digit")
+        }
+        else{
+            document.getElementById('emgamountneeded').innerHTML=" "
             setStep(8)
-
-        } 
-        else if (mgamountneeded == null || mgamountneeded === '' || mgamountneeded.length < 1  || mgamountneeded.length > 8) {
-            alert("Enter Amount Needed !  Length of atleast of 1 digit and less than 8 digit")
+        }
+        if (mgmedicalamount == null || mgmedicalamount === '') {
+            document.getElementById('emgmedicalamount').innerHTML="Please Enter Medical Amount !"
             setStep(8)
-
-        } 
-        // else if (mgamtcontri == null || mgamtcontri === '' || mgamtcontri.length < 2  || mgamtcontri.length > 8) {
-        //     alert("Enter MG Amount Contribution Self !  Length of atleast of 2 digit and less than 8 digit")
-        //     setStep(8)
-
-        // } 
-        // else if (mgexistingmedaid == null || mgexistingmedaid === '' || mgexistingmedaid.length < 2  || mgexistingmedaid.length > 50) {
-        //     alert("Enter MG Existing Medical Aid !  Length of atleast of 2 characters and less than 50 characters")
-        //     setStep(8)
-
-        // } 
-        else if (mgmedicalamount == null || mgmedicalamount === '' || mgmedicalamount.length < 1  || mgmedicalamount.length > 8) {
-            alert("Enter Medical Amount !  Length of atleast of 1 digit and less than 8 digit")
+        }
+        else if(mgmedicalamount.length < 1  || mgmedicalamount.length > 8){
+            document.getElementById('emgmedicalamount').innerHTML="Minimum 1 and Maximum 8 digits are allowed !"
             setStep(8)
-
+        }
+        else{
+            document.getElementById('emgmedicalamount').innerHTML=" "
+            setStep(8)
         } 
-        // else if (mgmedinsurance == null || mgmedinsurance === '' || mgmedinsurance.length < 2  || mgmedinsurance.length > 50) {
-        //     alert("Enter MG Medical Insurance !  Length of atleast of 2 characters and less than 50 characters")
-        //     setStep(8)
-
-        // } 
+        if( mghospitalname == null || mghospitalname === '' || mghospitalname.length < 2  || mghospitalname.length > 50 || mghospitaladdress == null || mghospitaladdress === '' || mghospitaladdress.length < 2  || mghospitaladdress.length > 50 || mgdiseasedetails == null || mgdiseasedetails === '' || mgdiseasedetails.length < 2  || mgdiseasedetails.length > 50 || mgaprxexpenses == null || mgaprxexpenses === '' || mgaprxexpenses.length < 1  || mgaprxexpenses.length > 8 || mgamountneeded == null || mgamountneeded === '' || mgamountneeded.length < 1  || mgamountneeded.length > 8 || mgmedicalamount == null || mgmedicalamount === '' || mgmedicalamount.length < 1  || mgmedicalamount.length > 8){
+            document.getElementById('invalid').innerHTML="Invalid Input !!"
+            setStep(8)
+        } 
         else {
+            document.getElementById('invalid').innerHTML=" "
             setStep(9)
         }
     }
@@ -110,6 +141,10 @@ export default function EighthStep() {
                     autoComplete="off"
                 >
                     <div>
+                        <div>
+                            <span id='invalid' class="text-danger size font-weight-bold"></span>
+                        </div>
+                        <br></br>
                         <div><h6 id="edgl1" >1. Unemployed Person : </h6></div>
                         <div>
                             <TextField label="Education" id='mgeducation' value={userData['mgunemployedpersoneducation']} onChange={(e) => setUserData({ ...userData, "mgunemployedpersoneducation": e.target.value })} margin='normal' variant='standard' color="primary" /><span>  </span>
@@ -167,14 +202,14 @@ export default function EighthStep() {
                         <div>
                             <div><h6 id="edgl1" >3. Patient Details : </h6></div>
 
-                            <TextField label="Hospital Name" id='mghospitalname' className ="required" value={userData['mghname']} onChange={(e) => setUserData({ ...userData, "mghname": e.target.value })} margin='normal' variant='standard' color="primary" /><span>  </span>
-                            <TextField label="Hospital Address" id='mghospitaladdress' className ="required" value={userData['mghaddress']} onChange={(e) => setUserData({ ...userData, "mghaddress": e.target.value })} margin='normal' variant='standard' color="primary" />
-                            <TextField label="Disease Details" id='mgdiseasedetails' className ="required" value={userData['mgdiseasedetails']} onChange={(e) => setUserData({ ...userData, "mgdiseasedetails": e.target.value })} margin='normal' variant='standard' color="primary" />
-                            <TextField label="Approx. Expenses" id='mgaprxexpenses' className ="required" type="number" value={userData['mgaprxexpenses']} onChange={(e) => setUserData({ ...userData, "mgaprxexpenses": e.target.value })} margin='normal' variant='standard' color="primary" />
-                            <TextField label="Amount Needed" id='mgamountneeded' className ="required" type="number" value={userData['mgamountneeded']} onChange={(e) => setUserData({ ...userData, "mgamountneeded": e.target.value })} margin='normal' variant='standard' color="primary" /><span>  </span>
+                            <TextField label="Hospital Name" id='mghospitalname' className ="required" value={userData['mghname']} onChange={(e) => setUserData({ ...userData, "mghname": e.target.value })} margin='normal' variant='standard' color="primary" /><br></br> <span id='emghospitalname' className='text-danger' >  </span>
+                            <TextField label="Hospital Address" id='mghospitaladdress' className ="required" value={userData['mghaddress']} onChange={(e) => setUserData({ ...userData, "mghaddress": e.target.value })} margin='normal' variant='standard' color="primary" /><br></br> <span id='emghospitaladdress' className='text-danger' >  </span>
+                            <TextField label="Disease Details" id='mgdiseasedetails' className ="required" value={userData['mgdiseasedetails']} onChange={(e) => setUserData({ ...userData, "mgdiseasedetails": e.target.value })} margin='normal' variant='standard' color="primary" /><br></br> <span id='emgdiseasedetails' className='text-danger' >  </span>
+                            <TextField label="Approx. Expenses" id='mgaprxexpenses' className ="required" type="number" value={userData['mgaprxexpenses']} onChange={(e) => setUserData({ ...userData, "mgaprxexpenses": e.target.value })} margin='normal' variant='standard' color="primary" /><br></br> <span id='emgaprxexpenses' className='text-danger' >  </span>
+                            <TextField label="Amount Needed" id='mgamountneeded' className ="required" type="number" value={userData['mgamountneeded']} onChange={(e) => setUserData({ ...userData, "mgamountneeded": e.target.value })} margin='normal' variant='standard' color="primary" /><br></br> <span id='emgamountneeded' className='text-danger' >  </span>
                             <TextField label="Amount Contributed(self)" type="number" id='mgamtcontri' value={userData['mgamtcont']} onChange={(e) => setUserData({ ...userData, "mgamtcont": e.target.value })} margin='normal' variant='standard' color="primary" />
                             <TextField label="Any existing medical aid" id='mgexistingmedaid' value={userData['mgmedicalaid']} onChange={(e) => setUserData({ ...userData, "mgmedicalaid": e.target.value })} margin='normal' variant='standard' color="primary" />
-                            <TextField label="Medical Amount" className ="required" type="number" id='mgmedicalamount' value={userData['mgmedamt']} onChange={(e) => setUserData({ ...userData, "mgmedamt": e.target.value })} margin='normal' variant='standard' color="primary" />
+                            <TextField label="Medical Amount" className ="required" type="number" id='mgmedicalamount' value={userData['mgmedamt']} onChange={(e) => setUserData({ ...userData, "mgmedamt": e.target.value })} margin='normal' variant='standard' color="primary" /><br></br> <span id='emgmedicalamount' className='text-danger' >  </span>
                             <TextField label="Medical Insurance Details" id='mgmedinsurance' value={userData['mginsurancedetails']} onChange={(e) => setUserData({ ...userData, "mginsurancedetails": e.target.value })} margin='normal' variant='standard' color="primary" />
                         </div><br/>
                             
