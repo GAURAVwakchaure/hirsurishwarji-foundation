e3a884795069b08a2e471624cf39205400a59f36
Ratnesh kumar Pandey <ratneshkumarpandey76@gmail.com>
2022-01-30 19:56:02 +0530

    small change in step1 view button
    done

--- client/src/Components/Application.js
+++ client/src/Components/Application.js
@@ -73,7 +73,7 @@ export default function Application() {
       
         return <div>
             
-            <button type="button" class="btn btn-outline-info btn-sm" onClick={()=> window.open(`http://localhost:3000/${row._id}`, "_blank")}>View</button>
+            <button type="button" class="btn btn-outline-info btn-sm" onClick={()=> window.open(`https://hirsurishwarji-foundation.herokuapp.com/${row._id}`, "_blank")}>View</button>
         </div>
     }
     // filter: textFilter() 
