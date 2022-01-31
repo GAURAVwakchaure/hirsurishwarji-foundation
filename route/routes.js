5e1c761df2541dc92e4d13367cc859b2e8fb36da
Ratnesh kumar Pandey <ratneshkumarpandey76@gmail.com>
2022-01-31 09:36:14 +0530

    undo route.js

--- route/routes.js
+++ route/routes.js
@@ -1,6 +1,5 @@
 
 import express, { Router } from 'express';
-import View from '../client/src/Components/View.js';
 import { getApplicantList,createApplication,getUserById} from '../controller/getRequest.js';
 
 const route = express.Router()
@@ -11,7 +10,7 @@ route.get('/ApplicantList',getApplicantList)
 
 route.get('/:id',getUserById)
 
-route.get('/user/:id', View)
+
 
 route.post('/createApplication',createApplication)
 
