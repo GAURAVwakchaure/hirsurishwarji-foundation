27a80715221069bacb6a4e5480152bd3a345af6b
Ratnesh kumar Pandey <ratneshkumarpandey76@gmail.com>
2022-01-31 09:32:32 +0530

    changes in route.js
    line no 14 added

--- route/routes.js
+++ route/routes.js
@@ -1,5 +1,6 @@
 
 import express, { Router } from 'express';
+import View from '../client/src/Components/View.js';
 import { getApplicantList,createApplication,getUserById} from '../controller/getRequest.js';
 
 const route = express.Router()
@@ -9,6 +10,8 @@ const route = express.Router()
 route.get('/ApplicantList',getApplicantList)
 
 route.get('/:id',getUserById)
+
+route.get('/user/:id', View)
 
 route.post('/createApplication',createApplication)
 
